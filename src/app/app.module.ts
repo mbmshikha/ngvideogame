5d5eb0e1c9976543786974ec4a924756964eddbe
shikhasystem <shikha.gupta@scientificgames.com>
2022-01-24 17:59:46 +0530

    Icon, form filed,tabs module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatModuleModule } from './module/mat-module/mat-module.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/module/mat-module/mat-module.module.ts
+++ src/app/module/mat-module/mat-module.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormField, MatFormFieldModule} from '@angular/material/form-field';
+
+
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatTabsModule,
+    MatIconModule,
+    MatFormFieldModule
+    ],
+    exports:[
+      MatFormField,
+      MatIconModule,
+      MatTabsModule
+    ]
 })
 export class MatModuleModule { }
